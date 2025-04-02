231191a21bb20a995b2fdec3f3fe4f5b6acd22ea
Gustavo Lacerda <gustavo4kps4@gmail.com>
2025-04-02 14:20:14 -0300

    Update TiposPrimitivos.java

--- TiposPrimitivos.java
+++ TiposPrimitivos.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.tiposprimitivos;
-/** Primeiro projeto java no laboratÃ³rio 111
+/** Primeiro projeto java no laboratorio 111
  */
 /**
  *
