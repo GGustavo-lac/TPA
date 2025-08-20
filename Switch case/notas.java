fa10db95067a52044d4dca0119046a1a842c6b70
Gustavo Lacerda <gustavo4kps4@gmail.com>
2025-08-20 14:51:15 -0300

    Add files via upload

---
+++ Switch case/notas.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.notas;
+
+import java.untill.Scanner;
+public class notas {
+    public static void main(String[args]){
+        Scanner inUser=nextInt();
+        
+    }
+}
